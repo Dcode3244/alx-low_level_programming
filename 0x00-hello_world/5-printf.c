4303fff0cfff4499b0715d104402a59b2fb394fd
Dcode3244 <dagimourgie111@gmail.com>
2022-06-29 17:43:16 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the text inside the printf function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
