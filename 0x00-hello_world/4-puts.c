9f5f37e657d57fe6425e73542682dca95912834d
Dcode3244 <dagimourgie111@gmail.com>
2022-06-29 17:31:57 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints the given text'
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
