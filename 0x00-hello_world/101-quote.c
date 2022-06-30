ae65b18e11be17d207ce8b869f51be17470b25d0
Dcode3244 <dagimourgie111@gmail.com>
2022-06-30 13:33:03 +0300

    advanced task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Return: always 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
