e4e33084e7ebb3c07fae93d8a76bd8df33f7eb70
Dcode3244 <dagimourgie111@gmail.com>
2022-07-01 16:54:25 +0300

    added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entrance point
+ *
+ * Description: prints lower case alphabets from a - z
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
+
