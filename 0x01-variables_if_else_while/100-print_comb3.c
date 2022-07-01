877af892e275b4fcfb8838d66e1410dcd8d5054f
Dcode3244 <dagimourgie111@gmail.com>
2022-07-01 19:29:03 +0300

    modified advanced task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,39 @@
 #include<stdio.h>
 
-int main()
+/**
+ * main - Entrance point
+ *
+ * Description: prints all all possible different combinations of two digits
+ * the two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * prints only the smallest combination of two digits
+ *
+ * Return: 0
+ **/
+
+int main(void)
 {
-int b = 1;
-for (int a = 0; a < 10; a++)
-{
-	b = a + 1;
-	while (b < 10)
+	int b = 1;
+	int a = 0;
+
+	while (a < 10)
 	{
-		putchar(a + '0');
-		putchar(b + '0');
-		if(a == 8 && b == 9)
+		b = a + 1;
+		while (b < 10)
 		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if(a == 8 && b == 9)
+			{
+				b++;
+				putchar('\n');
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
 			b++;
-			putchar('\n');
-			continue;
 		}
-		putchar(',');
-		putchar(' ');
-		b++;
+		a++;
 	}
-}
+	return (0);
 }
