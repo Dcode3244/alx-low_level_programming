6e956e8d664a426709ee7c41608800293043f33a
Dcode3244 <dagimourgie111@gmail.com>
2022-07-05 20:54:18 +0300

    added advanced task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * -  finds the last digit of a number
+ *
+ * @n: input integer
+ *
+ * Description - prints the last digit of an integer
+ *
+ * Return: last digit
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int sum;
+	while (n < 1024)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			sum = sum + n;
+		}
+		n++;
+	}
+	printf("%d\n", sum);
+}
