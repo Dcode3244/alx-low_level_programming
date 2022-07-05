63feb4c8595bc5228bec66083796356ce2f54abb
Dcode3244 <dagimourgie111@gmail.com>
2022-07-05 13:16:08 +0300

    added task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');	
+	putchar('\n');
+	return(1);
+}
