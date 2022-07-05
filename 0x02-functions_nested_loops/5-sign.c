5f2878aa6cc05a2cb118049f9185f3674379f7b3
Dcode3244 <dagimourgie111@gmail.com>
2022-07-05 16:33:44 +0300

    added task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+/**
+ * print_sign - my function
+ *
+ * @n: the character to be checked
+ *
+ * Description - checks sign of character
+ *
+ * Return: 1 if is positive, -1 if negative, else 0
+ *
+ */
+
+int print_sign(int n)
+{
+	while (n > 0)
+		return (1);
+	while (n < 0)
+		return (-1);
+	return (0);
+}
