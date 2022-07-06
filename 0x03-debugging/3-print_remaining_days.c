379b47f714dfb29cb3105e1cdb0706a198f35f4f
Dcode3244 <dagimourgie111@gmail.com>
2022-07-06 14:53:20 +0300

    fixed task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,7 +18,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-		if ((year % 100 == 0) && (year % 400 != 0))
+		if (((year % 100 == 0) && (year % 400 != 0)) || (year < 0))
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 			return;
@@ -28,7 +28,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if ((month == 2 && day == 60) || year < 0)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
