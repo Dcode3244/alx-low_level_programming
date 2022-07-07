e7d348a50be2b5f93d2e43cd2fc19a67695d7f4e
Dcode3244 <dagimourgie111@gmail.com>
2022-07-07 14:15:08 +0300

    added task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9 followed
+ * by a new line
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
