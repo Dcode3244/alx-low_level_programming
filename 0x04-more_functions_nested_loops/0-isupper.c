215f65468779b1d3b9d69f0d1bcf2dc382a59fd5
Dcode3244 <dagimourgie111@gmail.com>
2022-07-07 13:23:31 +0300

    added task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if char c is upper case
+ * @c: character to be checked
+ * Return: 1 if uppercase, 0 if not
+ */
+
+int _isupper(char c)
+{
+	if (c <= 'Z' && c >= 'A')
+		return (1);
+	return (0);
+}
