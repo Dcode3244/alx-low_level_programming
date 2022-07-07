c9a42202bf3846b18c189c4d4fef769c067a83d9
Dcode3244 <dagimourgie111@gmail.com>
2022-07-07 13:25:19 +0300

    modified task 0 and main.h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,7 +6,7 @@
  * Return: 1 if uppercase, 0 if not
  */
 
-int _isupper(char c)
+int _isupper(int c)
 {
 	if (c <= 'Z' && c >= 'A')
 		return (1);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,2 @@
 int _putchar(char c);
-int _isupper(char c);
+int _isupper(int c);
