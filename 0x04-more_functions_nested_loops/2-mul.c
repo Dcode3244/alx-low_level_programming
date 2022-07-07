f047bf6f2e76a897f6ca4d441315277a19aab5b3
Dcode3244 <dagimourgie111@gmail.com>
2022-07-07 14:05:42 +0300

    added task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intege
+ * @a: character to be multiplied
+ * @b: character to be multiplied
+ * Return: multiples of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
