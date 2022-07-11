20f97c5778db23ba6b95cfae5bd794515a332672
Dcode3244 <dagimourgie111@gmail.com>
2022-07-11 15:30:11 +0300

    added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line
+ *
+ * @str : string to be printed out
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
