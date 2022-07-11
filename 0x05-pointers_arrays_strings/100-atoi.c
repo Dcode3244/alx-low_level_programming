54990a993f749a2152a6230b3c9011bd35ac7b6d
Dcode3244 <dagimourgie111@gmail.com>
2022-07-11 23:21:58 +0300

    modified advanced task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,35 +10,23 @@
 
 int _atoi(char *s)
 {
-	int i = 0, len = 0, j = 0;
-	unsigned int num, pow, neg;
+
+	int sign = 1, i = 0;
+	unsigned int n = 0;
+
 
 	while (s[i] != '\0')
 	{
-		len++;
+		if (s[i] == '-')
+			sign *= -1;
+
+		else if (s[i] >= '0' && s[i] <= '9')
+			n = (s[i] - '0') + (n * 10);
+
+		else if (n > 0)
+			break;
 		i++;
 	}
 
-	j = len - 1;
-	i = 0;
-
-	num = 0;
-	pow = 1;
-	neg = 0;
-	while (j >= 0)
-	{
-		if (s[j] == 45)
-		{
-			neg++;
-		}
-		if (s[j] >= 48 && s[j] <= 57)
-		{
-			num = num + (s[j] - 48) * pow;
-			pow = pow * 10;
-		}
-		j--;
-	}
-	if (neg % 2 != 0)
-		return (-num);
-	return (num);
+	return (n * sign);
 }
