6fc0d822c33295343ae75e5a8c5e6f2946790b07
Dcode3244 <dagimourgie111@gmail.com>
2022-07-11 22:32:52 +0300

    added advanced task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * _atoi - converts string to integer
+ *
+ * @s: string to be converted
+ *
+ * Return: convered integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, len = 0, j = 0;
+	int num, pow, neg;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	j = len - 1;
+	i = 0;
+
+	num = 0;
+	pow = 1;
+	neg = 0;
+	while (j >= 0)
+	{
+		if (s[j] == 45)
+		{
+			neg++;
+		}
+		if (s[j] >= 48 && s[j] <= 57)
+		{
+			num = num + (s[j] - 48) * pow;
+			pow = pow * 10;
+		}
+		j--;
+	}
+	if (neg % 2 != 0)
+		return (-num);
+	return (num);
+}
