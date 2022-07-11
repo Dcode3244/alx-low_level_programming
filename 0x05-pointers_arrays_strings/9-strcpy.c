f13bb8553f71b71240b9578431fa85e2eef2894b
Dcode3244 <dagimourgie111@gmail.com>
2022-07-11 20:13:25 +0300

    modified task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,17 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] != 0)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i + 1] = "\0";
+
+	while (src[i] == '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
 	return (dest);
 }
