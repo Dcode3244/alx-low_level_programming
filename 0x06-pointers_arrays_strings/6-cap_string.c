ca1cbe22dd4c688c5757167a76bb8dbd60aca1ec
Dcode3244 <dagimourgie111@gmail.com>
2022-07-13 20:30:59 +0300

    added task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @s: string to be capitalized
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	int isseparator = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (isseparator == 1)
+			{
+				s[i] -= 32;
+				isseparator = 0;
+			}
+		}
+		else
+		{
+			if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))
+			{
+				i++;
+				isseparator = 0;
+				continue;
+			}
+			isseparator = 1;
+		}
+		i++;
+	}
+	return (s);
+}
