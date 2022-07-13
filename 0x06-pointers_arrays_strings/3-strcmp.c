12807e6f8143698cd8bf88040bdc7f603b43271d
Dcode3244 <dagimourgie111@gmail.com>
2022-07-13 18:34:58 +0300

    fixed task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,10 +18,11 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int val;
 
-	while (s1[i] && s2[i])
+	for (i = 0; s1[i] && s2[i]; i++)
 	{
 		val = s1[i] - s2[i];
-		i++;
+		if (val != 0)
+			return (val);
 	}
 	return (val);
 }
