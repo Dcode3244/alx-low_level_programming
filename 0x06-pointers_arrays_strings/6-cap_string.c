960cc7ab36def293b21c4e6ce799764eaee20c55
Dcode3244 <dagimourgie111@gmail.com>
2022-07-13 21:24:23 +0300

    modified task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 			i++;
 			continue;
 		}
-		else if (s[i - 1] == ' ' ||
+		if (i == 0 || s[i - 1] == ' ' ||
 			s[i - 1] == '\t' ||
 			s[i - 1] == '\n' ||
 			s[i - 1] == ',' ||
