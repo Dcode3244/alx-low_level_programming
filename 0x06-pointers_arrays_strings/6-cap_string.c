11161ccf6864900efdec73d3c3b075081c5475ea
Dcode3244 <dagimourgie111@gmail.com>
2022-07-13 21:17:45 +0300

    fixed task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,37 +11,30 @@
 char *cap_string(char *s)
 {
 	int i = 0;
-	int isseparator = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (!(s[i] >= 'a' && s[i] <= 'z'))
 		{
-			if (isseparator == 1)
-			{
-				s[i] -= 32;
-				isseparator = 0;
-			}
-		}
-		else if (s[i] == ' ' ||
-			s[i] == '\t' ||
-			s[i] == '\n' ||
-			s[i] == ',' ||
-			s[i] == ';' ||
-			s[i] == '.' ||
-			s[i] == '!' ||
-			s[i] == '?' ||
-			s[i] == '"' ||
-			s[i] == '(' ||
-			s[i] == ')' ||
-			s[i] == '{' ||
-			s[i] == '}')
-		{
-			isseparator = 1;
 			i++;
 			continue;
 		}
-		isseparator = 0;
+		else if (s[i - 1] == ' ' ||
+			s[i - 1] == '\t' ||
+			s[i - 1] == '\n' ||
+			s[i - 1] == ',' ||
+			s[i - 1] == ';' ||
+			s[i - 1] == '.' ||
+			s[i - 1] == '!' ||
+			s[i - 1] == '?' ||
+			s[i - 1] == '"' ||
+			s[i - 1] == '(' ||
+			s[i - 1] == ')' ||
+			s[i - 1] == '{' ||
+			s[i - 1] == '}')
+		{
+			s[i] -= 32;
+		}
 		i++;
 	}
 	return (s);
