7df4021097497d98f7cfcbabb5c56256d3b02888
Dcode3244 <dagimourgie111@gmail.com>
2022-07-13 18:50:42 +0300

    fixed task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, j;
-
-	for (i = 0; i < n / 2; i++, n--)
+	int i, j, len = n;
+	
+	for (i = 0; i < n / 2; i++, len--)
 	{
 		j = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = j;
+		a[i] = a[len - 1];
+		a[len - 1] = j;
 	}
 }
