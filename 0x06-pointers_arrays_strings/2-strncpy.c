b825578d83ff0abd6ef03701ebd8e59f29d6d348
Dcode3244 <dagimourgie111@gmail.com>
2022-07-13 18:18:12 +0300

    modified task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strncpy - a function that copies a string
  *
@@ -14,9 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && dest[i];  i++)
-	{
+	for (i = 0; i < n && dest[i] && src[i];  i++)
 		dest[i] = src[i];
-	}
+	for (; i < n ; i++)
+		dest[i] = '\0';
 	return (dest);
 }
