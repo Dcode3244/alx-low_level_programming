6dea971fc48beb9e200193fd42ef485de68c5487
Dcode3244 <dagimourgie111@gmail.com>
2022-07-14 11:48:02 +0300

    fixed advanced task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,12 +7,16 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = n * -1;
 	}
-	if (n / 10 >= 1)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	else
+		num = n;
+	if (num / 10 >= 1)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
