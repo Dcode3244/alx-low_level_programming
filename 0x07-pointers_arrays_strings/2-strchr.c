55c2234d6341891abb8ffbb0e222f66442e045c7
Dcode3244 <dagimourgie111@gmail.com>
2022-07-18 14:22:34 +0300

    modified task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,8 +16,8 @@ char *_strchr(char *s, char c)
 		if (s[i] == c)
 			return (&s[i]);
 	}
-	if (s[i] == '\0')
-		return (&s[i]);
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
-	return ('\0');
+	return (s + i);
 }
