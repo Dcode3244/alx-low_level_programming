86495c3396478ad63a3beed1be4e30f9670ad819
Dcode3244 <dagimourgie111@gmail.com>
2022-07-18 12:45:35 +0300

    modified task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,15 +12,7 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
-			if (a[i][j] >= 'a' && a[i][j] <= 'z')
 				_putchar(a[i][j]);
-			else if (a[i][j] >= 'A' && a[i][j] <= 'Z')
-				_putchar(a[i][j]);
-			else
-				_putchar(' ');
-			if (j == 7)
-				_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
