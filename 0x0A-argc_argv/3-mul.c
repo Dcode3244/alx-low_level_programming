d87270b89b44ceb9ce0b2c914538498d0f5b2723
Dcode3244 <dagimourgie111@gmail.com>
2022-07-22 12:47:32 +0300

    added task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: the command line arguments
+ * Return: 1 if argc is not 3, else 0;
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
