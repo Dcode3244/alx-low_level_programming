04593fcd382094a3d971d55ee6a58e2039958349
Dcode3244 <dagimourgie111@gmail.com>
2022-07-23 16:31:02 +0300

    modified task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,12 +13,19 @@ char *str_concat(char *s1, char *s2)
 	int i, len1, len2;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	len1 = len2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		len1++;
+
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s2[i] != '\0'; i++)
 		len2++;
@@ -28,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		p[i] = s1[i];
 
 	for (i = 0; i <= len2; i++)
