72e50d62e3f985d2f125db6cb9fdcc69d0663f68
Dcode3244 <dagimourgie111@gmail.com>
2022-07-23 17:42:00 +0300

    modified task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,6 +15,9 @@ int **alloc_grid(int width, int height)
 
 	p  = malloc((height) * sizeof(int *));
 
+	if (p == NULL)
+		return NULL;
+
 	for (i = 0; i < height; i++)
 		p[i] = malloc(width * sizeof(int));
 
