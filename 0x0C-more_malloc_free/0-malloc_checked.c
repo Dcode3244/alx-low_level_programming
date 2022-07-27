7715ab7abf1b5c548f0d61f85162b61b5072e51d
Dcode3244 <dagimourgie111@gmail.com>
2022-07-27 21:08:38 +0300

    added task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates momory using malloc
+ * @b: size of memory to be allocated
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
