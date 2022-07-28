7f476b14780395de4fead16b32af70ac6c448f5f
Dcode3244 <dagimourgie111@gmail.com>
2022-07-28 11:22:32 +0300

    modified task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *s;
 	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	s = malloc(nmemb * size);
+
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
 		s[i] = 0;
