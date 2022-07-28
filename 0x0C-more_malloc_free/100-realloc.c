d66615894b05e47021a4e901d789f8b64b038ae0
Dcode3244 <dagimourgie111@gmail.com>
2022-07-28 14:32:39 +0300

    modified task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,6 +19,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 		return (NULL);
 
+	if (new_size < old_size)
+		return (ptr);
+
 	s = malloc(new_size);
 
 	if (s == NULL)
