82b520eee27a3e576184f164a3d9984311142855
Dcode3244 <dagimourgie111@gmail.com>
2022-08-01 15:49:07 +0300

    added task 0 and README file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+## dog.h
+### Define a new type struct dog with the following elements:
+name, type = char *
+age, type = float
+owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data type
+ * @name: name of the dog
+ * @age:cage of the dog
+ * @owner: owner of the dog
+ * Description: a dog datatype including three members
+ */
+
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
+
+#endif
