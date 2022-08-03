23ea32f5c3d3e994f3cfe2d11069c6a09dc4eefc
Dcode3244 <dagimourgie111@gmail.com>
2022-08-03 21:34:43 +0300

    added advanced task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opicodes of its own main function
+ * @ac: number of command line arguments
+ * @av: the command line arguments
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	int i, num;
+	int (*p_main)(int, char **);
+	unsigned char op;
+
+	p_main = main;
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num = atoi(av[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < num; i++)
+	{
+		op = *(unsigned char *)p_main;
+		printf("%.2x", op);
+		if (i != num)
+			printf(" ");
+		p_main++;
+	}
+
+	printf("\n");
+	return (0);
+}
