0f9ccee6be5d5cfd11a12cbc3761b7e0d01ef103
Dcode3244 <dagimourgie111@gmail.com>
2022-08-04 14:14:49 +0300

    modified header file

--- 0x10_variadic_functions/variadic_functions.h
+++ 0x10_variadic_functions/variadic_functions.h
@@ -7,4 +7,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif VARIADIC_H
+#endif
