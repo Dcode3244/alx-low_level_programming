cd1ed4e9b322f1d204cb7d9d851829267825abc5
Dcode3244 <dagimourgie111@gmail.com>
2022-08-11 17:35:33 +0300

    added task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include "stdio.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: the list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i, node = 0;
+	char *null = "[0] (nil)";
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			while (*null)
+				_putchar(*null++);
+		}
+		else
+		{
+			_putchar('[');
+			_putchar(h->len + '0');
+			_putchar(']');
+			_putchar(' ');
+			for (i = 0; (h->str)[i] != '\0'; i++)
+				_putchar((h->str)[i]);
+		}
+		_putchar('\n');
+		node++;
+		h = h->next;
+	}
+
+	return (node);
+}
