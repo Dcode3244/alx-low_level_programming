558dc94610c0c94942d6edfcd9397ba82075bdac
Dcode3244 <dagimourgie111@gmail.com>
2022-08-12 00:13:22 +0300

    modified advanced taks 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,20 +3,16 @@ extern printf
 section .text
    global main
 
-main:   
+main:
           lea       rdi, [rel fmt]
           lea       rsi, [rel msg]
-              
+
           mov       al, 0
           call      printf
 
           mov       rax, 0x3C
           xor       rdi, rdi
           syscall
-
-          section   .data
-
-
 
 section .data
    msg: db "Hello, Holberton",10, 0
