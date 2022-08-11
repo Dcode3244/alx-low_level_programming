bfc5df85e85a77b872d7a04bf9ae7489709ae288
Dcode3244 <dagimourgie111@gmail.com>
2022-08-11 18:32:10 +0300

    added task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - prints all the elements of a list_t list
+ * @h: the list
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+		node++;
+
+	return (node);
+}
