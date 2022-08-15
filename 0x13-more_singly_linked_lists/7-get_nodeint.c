dd1f6032966d0ba40ef9ea9a4a2b8a7167697838
Dcode3244 <dagimourgie111@gmail.com>
2022-08-15 18:31:04 +0300

    modified task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp, *new;
 	unsigned int i, node = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = head;
 
 	while (new)
