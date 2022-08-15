61d819bc0a2048b250ccb5c9c6219d0166501112
Dcode3244 <dagimourgie111@gmail.com>
2022-08-15 12:23:59 +0300

    added task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_list
+ * @head: pointer to the first list node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
