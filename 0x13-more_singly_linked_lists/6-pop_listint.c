02737faa3af87e090506cf0d18d20a4b58551a7f
Dcode3244 <dagimourgie111@gmail.com>
2022-08-15 18:29:18 +0300

    modified task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,11 +10,12 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int n = (*head)->n;
+	int n;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 
+	n = (*head)->n;
 	temp = *head;
 	*head = (*head)->next;
 	free(temp);
