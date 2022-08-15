4285b2376e0bae5358c2f7c2329ff0a52c3acdb3
Dcode3244 <dagimourgie111@gmail.com>
2022-08-15 11:21:07 +0300

    removed some file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+	int nodes = 0;
+
+	if (h = NULL)
+	{
+		return (NULL);
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# alx-low_level_programming
-alx-low_level_programming projects
