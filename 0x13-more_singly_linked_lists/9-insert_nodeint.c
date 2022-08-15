f4736d9a2d51cd00c0905c90d3561a6ac339b48c
Dcode3244 <dagimourgie111@gmail.com>
2022-08-15 19:16:34 +0300

    modified task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (i = 0; i < idx - 1; i++)
 	{
-		if (temp == NULL)
+		if (temp == NULL || temp->next == NULL)
 			return (NULL);
 		temp = temp->next;
 	}
