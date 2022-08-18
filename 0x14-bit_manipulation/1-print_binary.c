d912b07e6411c35f67396a4ad88891ecf773cf99
Dcode3244 <dagimourgie111@gmail.com>
2022-08-18 12:08:47 +0300

    added task 0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number
+ */
+
+void print_bianry(unsigned long int n)
+{
+	
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+## 0-binary_to_uint.c
+- converts a binary number to an unsigned int
