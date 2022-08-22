83a54f4d33ff5f8887ae725e90645027515a6f50
Dcode3244 <dagimourgie111@gmail.com>
2022-08-22 19:47:31 +0300

    modified task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	wr = write(fd, text_content, strlen(text_content));
+	if (text_content == NULL)
+		wr = write(fd, text_content, strlen(text_content));
 
 	if (fd < 0 || wr < 0)
 		return (-1);
