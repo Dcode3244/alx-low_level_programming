ddf742b6e364bb7b4181801a200ba8526d95a6ec
Dcode3244 <dagimourgie111@gmail.com>
2022-08-22 19:38:26 +0300

    modified task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,13 +11,8 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, wr;
-	char *buf;
 
 	if (filename == NULL)
-		return (-1);
-
-	buf = malloc(sizeof(char) * strlen(text_content));
-	if (buf == NULL)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
@@ -27,7 +22,6 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0 || wr < 0)
 		return (-1);
 
-	free(buf);
 	close(fd);
 
 	return (1);
