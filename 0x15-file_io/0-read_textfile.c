d642117aab2b346e86b2942edf7f760137523ef2
Dcode3244 <dagimourgie111@gmail.com>
2022-08-22 18:28:58 +0300

    added README file and task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_textfile - reads a text file an dprints it to the POSIX standard output
+ * @filename: the text file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ * or 0 if the file can not be opened or if filename is NULL or
+ * if write fails or does not write the expected amount of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t sz;
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+	{
+		return (0);
+	}
+
+	sz = read(fd, buf, letters);
+	buf[sz] = '\0';
+
+	close(fd);
+	printf("%s\n", buf);
+	return (sz);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+## 0-read_textfile.c
+- reads a text file and prints it to the POSIX standard output.
