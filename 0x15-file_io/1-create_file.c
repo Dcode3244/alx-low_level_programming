906243d3a08c64a879e9a9f1eb1b1cc8030137eb
Dcode3244 <dagimourgie111@gmail.com>
2022-08-22 23:16:52 +0300

    modified task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * create_file - creates a file
  * @filename: the file name
@@ -10,14 +10,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr;
+	int fd, wr, text_len, i;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_len = 0;
+	else
+	{
+		for (i = 0, text_len = 0; text_content[i]; i++)
+			text_len++;
+	}
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (text_content != NULL)
-		wr = write(fd, text_content, strlen(text_content));
+	wr = write(fd, text_content, text_len);
 
 	if (fd < 0 || wr < 0)
 		return (-1);
