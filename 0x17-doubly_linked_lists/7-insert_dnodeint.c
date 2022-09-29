0df7dca97bbc22b108d43718e7e2226478aa5612
Dcode3244 <dagimourgie111@gmail.com>
2022-09-29 12:39:34 +0300

    bug fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,8 +29,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 
 	if (idx >= len)
+	{
+		free(new);
 		return (NULL);
-
+	}
 	temp = *h;
 	for (i = 0; i < idx; i++)
 		temp = temp->next;
