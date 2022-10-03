21d5e04a62985d627f902fd6933a65dd6270d1dc
Dcode3244 <dagimourgie111@gmail.com>
2022-10-04 00:02:41 +0300

    bug fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O $HOME/libinjection.so https://github.com/Dcode3244/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinjection.so
+wget -q -O $HOME/libinjection.so https://github.com/Dcode3244/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libinjection.so
 export LD_PRELOAD=$HOME/libinjection.so
