df1ff87cb8891e8a66cee7aa082dab96ba70f108
Dcode3244 <dagimourgie111@gmail.com>
2022-10-20 20:55:55 +0300

    style fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,12 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash;
 
 	hash = malloc(sizeof(hash_table_t));
-
-	if (hash == NULL || size == 0)
+	if (hash == NULL)
 		return (NULL);
 
 	hash->size = size;
-	hash->array = malloc(size * sizeof(hash_node_t));
+	hash->array = malloc(size * sizeof(hash_node_t *));
 
 	return (hash);
 }
