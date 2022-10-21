a934dbd9994d28da99874048d2a86c41b7168fd9
Dcode3244 <dagimourgie111@gmail.com>
2022-10-21 17:31:22 +0300

    memory leak fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,44 +12,41 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	hash_node_t *item, *temp;
-	char *value_cp;
+	hash_node_t *item;
 
 	if (key == NULL || *key == '\0' || ht == NULL || value == NULL)
 		return (0);
-	idx = key_index((unsigned char *)key, ht->size);
-	value_cp = strdup(value);
-	if (value_cp == NULL)
-		return (0);
+
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
 		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
 	item->key = strdup(key);
 	if (item->key == NULL)
 	{
-		free(value_cp);
 		free(item);
 		return (0);
 	}
-	item->value = value_cp;
-	item->next = NULL;
+	item->value = strdup(value);
+	if (item->value == NULL)
+	{
+		free(item->key);
+		free(item);
+		return (0);
+	}
 	if (ht->array[idx] == NULL)
+	{
 		ht->array[idx] = item;
+		ht->array[idx]->next = NULL;
+	}
+	else if (strcmp(ht->array[idx]->key, key) == 0)
+		strcpy(ht->array[idx]->value, value);
 	else
 	{
-		temp = ht->array[idx];
-		while (strcmp(temp->key, key) != 0 && temp->next != NULL)
-			temp = temp->next;
-		if (strcmp(temp->key, key) == 0)
-		{
-			free(item->key);
-			free(item);
-			free(temp->value);
-			temp->value = value_cp;
-			return (1);
-		}
 		item->next = ht->array[idx];
-		ht->array[idx]->next = item;
+		ht->array[idx] = item;
 	}
+
 	return (1);
 }
