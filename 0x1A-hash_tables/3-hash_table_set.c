82c27c559de12096e4c3bbb8fff0a4bbbea5a007
Dcode3244 <dagimourgie111@gmail.com>
2022-10-21 11:19:49 +0300

    bug fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	idx = key_index((unsigned char *)key, ht->size);
 
 	item->key = strdup(key);
+	if (item->key == NULL)
+		return (0);
+
 	item->value = strdup(value);
+	if (item->value == NULL)
+		return (0);
 
 	if (ht->array[idx] == NULL)
 	{
