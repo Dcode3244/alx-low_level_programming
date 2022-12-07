2f5c577cb0e11f3cbc2fde7934997b2bf8526950
Dcode3244 <Dagimourgie111@gmail.com>
2022-12-07 20:41:11 +0300

    bug fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,24 +13,24 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                try:
+                if j + 1 > len(grid[0]) - 1:
+                    count += 1
+                else:
                     if grid[i][j + 1] != 1:
                         count += 1
-                except IndexError:
+                if j - 1 < 0:
                     count += 1
-                try:
+                else:
                     if grid[i][j - 1] != 1:
                         count += 1
-                except IndexError:
+                if i - 1 < 0:
                     count += 1
-                try:
+                else:
                     if grid[i - 1][j] != 1:
                         count += 1
-                except IndexError:
+                if i + 1 > len(grid) - 1:
                     count += 1
-                try:
+                else:
                     if grid[i + 1][j] != 1:
                         count += 1
-                except IndexError:
-                    count += 1
     return count
